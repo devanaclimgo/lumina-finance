ed5ecd9160b964cb31a64d01a09947a623404999
Ana <anaclimgo@gmail.com>
2026-08-20 19:16:26 -0300

    feat: add routing for analytics component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "analytics",
+    loadComponent: () =>
+      import("./components/analytics/analytics.component").then((m) => m.AnalyticsPageComponent),
+  },
+];
